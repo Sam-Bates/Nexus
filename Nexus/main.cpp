53d2652e449be99e35e231a20b3d8c8609a8ce3a
Sam-Bates <batez1993@gmail.com>
2015-10-15 20:33:34 +1300

    A* partially done

--- Nexus/main.cpp
+++ Nexus/main.cpp
@@ -3,6 +3,7 @@
 #include "graphics.h"
 #include "board.h"
 #include "gridVector.h"
+#include <vector>
 
 int main(int argc, char* args[])
 {
@@ -100,6 +101,62 @@ int main(int argc, char* args[])
 							//revert colour of selected ball
 							board[x][y] = selectedPieceType;
 
+							//A*
+							std::vector<int> openList;
+							std::vector<int> closedList;
+							int orignalSquare = 0; // find original square
+							int destinationSquare = ((BOARD_WIDTH * y) + x);
+							openList.push_back((BOARD_WIDTH * y) + x); //add original square
+							do{
+								int currentSquareIndex = getLowestFscore();//search for lowest Fscore
+								closedList.push_back((BOARD_WIDTH * y) + x);
+								openList.erase(openList.begin() + getCurrentSquareIndex());
+
+								if (searchForIndex(destinationSquare))
+								{
+									break;
+								}
+
+								//adjacentSquares = 
+								
+							} while (!openList.empty());
+							//[openList add : originalSquare]; // start by adding the original position to the open list
+							
+							//do {
+							//	currentSquare = [openList squareWithLowestFScore]; // Get the square with the lowest F score
+
+							//	[closedList add : currentSquare]; // add the current square to the closed list
+							//	[openList remove : currentSquare]; // remove it to the open list
+
+							//	if ([closedList contains : destinationSquare]) { // if we added the destination to the closed list, we've found a path
+							//		// PATH FOUND
+							//		break; // break the loop
+							//	}
+
+							//	adjacentSquares = [currentSquare walkableAdjacentSquares]; // Retrieve all its walkable adjacent squares
+
+							//	foreach(aSquare in adjacentSquares) {
+
+							//		if ([closedList contains : aSquare]) { // if this adjacent square is already in the closed list ignore it
+							//			continue; // Go to the next adjacent square
+							//		}
+
+							//		if (![openList contains : aSquare]) { // if its not in the open list
+
+							//			// compute its score, set the parent
+							//			[openList add : aSquare]; // and add it to the open list
+
+							//		}
+							//		else { // if its already in the open list
+
+							//			// test if using the current G score make the aSquare F score lower, if yes update the parent because it means its a better path
+
+							//		}
+							//	}
+
+							//} while (![openList isEmpty]); // Continue until there is no more available square in the open list (which means there is no path)
+
+
 							//swap contents of cells
 							changeBall(x, y, selectedPieceType);
 							changeBall(selectedX, selectedY, POS_FREE);
@@ -126,4 +183,16 @@ int main(int argc, char* args[])
 	}
 	//close();
 	return 0;
+}
+int getLowestFscore()
+{
+	return 0;
+}
+int getCurrentSquareIndex()
+{
+	return 0;
+}
+bool searchForIndex(int index)
+{
+	return true;
 }
